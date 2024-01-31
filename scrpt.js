5b88c011fbe11d3a75a4be20ac2ea129b383b24f
aishacmela <aishacmela@gmail.com>
2024-01-31 23:18:18 +0200

    injected date data

--- scrpt.js
+++ scrpt.js
@@ -4,15 +4,41 @@ function updateWeather(response){
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
-    let iconElement = document.querySelector("#icon")
-    iconElement.innerHTML= `<img src="${response.data.condition.icon_url}" alt= class="weather-app-icon"
-            >`
+    let iconElement = document.querySelector("#icon");
+    let timeElement = document.querySelector("#time");
 
+    let date = new Date (response.data.time *1000);
+    timeElement.innerHTML = formatDate(new Date);
+    iconElement.innerHTML= `<img src="${response.data.condition.icon_url}" alt= class="weather-app-icon">`
     windElement.innerHTML = `${response.data.wind.speed}km/h`;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     descriptionElement.innerHTML = response.data.condition.description;
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+function formatDate(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    
+
+    let days = [
+     "Sunday",
+     "Monday", 
+     "Tuesday", 
+     "Wednesday",
+     "Thursday",
+     "Friday", 
+     "Saturday"
+    ];
+    let day = days[date.getDay()];
+    if (minutes <10){
+        minutes = `0${minutes}`
+    }
+    if (hours <10){
+        hours = `0${hours}`
+    }
+
+    return `${day} ${hours}:${minutes}`;
 }
 
 
