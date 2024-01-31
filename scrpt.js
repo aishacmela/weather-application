e4382d46e9f0dbc63b09128909b7eda82d97eba1
aishacmela <aishacmela@gmail.com>
2024-01-31 22:25:37 +0200

    updated search functionality using API

--- scrpt.js
+++ scrpt.js
@@ -1,10 +1,32 @@
+function updateWeather(response){
+    let temperatureElement = document.querySelector("#temperature-value")
+    let cityElement = document.querySelector("#city");
+
+   
+    
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current)
+
+
+
+}
+
+
+function searchcity(city){
+ let apiKey = `4ce023e4f48aaf8bd6a4cb7eo6dbd3ft`
+ let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
+axios.get(apiUrl).then(updateWeather)
+}
+
 
 function search(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-input");
-    let cityElement= document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+   
+    searchcity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
+
+searchcity("Cape Town")
